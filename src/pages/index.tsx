9711cf62f3196ca8bc6ddf819404c49ed5201232
Leonardo Garreto <leonardogarreto@gmail.com>
2022-02-22 23:00:47 -0300

    started index of pages and finished document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-
+import { ReactElement, ReactNode } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +24,15 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <>
+      <p>hello world</p>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient(process.env.PRISMIC_API_ENDPOINT, { req });
+  const postsResponse = await prismic.query();
+};
