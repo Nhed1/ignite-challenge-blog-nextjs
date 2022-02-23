0c856507513507d5b144f3bd9740e1a629114fad
Leonardo Garreto <leonardogarreto@gmail.com>
2022-02-23 11:19:12 -0300

    getStaticProps done, but not checked yet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement } from 'react';
+
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -33,5 +36,36 @@ export default function Home(): ReactElement {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient(process.env.PRISMIC_API_ENDPOINT, { req });
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: [
+        'posts.title',
+        'posts.subtitle',
+        'posts.author',
+        'posts.banner',
+        'posts.content',
+      ],
+      pageSize: 2,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.last_publication_date,
+      next_page: post.url,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  return {
+    props: { posts },
+  };
 };
